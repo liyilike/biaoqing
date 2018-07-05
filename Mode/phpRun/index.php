b4ffd4fdb3a8a1e498261b144e164741bc9ca32c
Unknown <liyiliyi.cool@gmail.com>
2018-07-05 18:14:11 +0800

    123

--- Mode/phpRun/index.php
+++ Mode/phpRun/index.php
@@ -3,7 +3,16 @@ header("ALLOW-CONTROL-ALLOW-ORIGIN:*");
 header("text/html;charset=UTF-8");
 header('Access-Control-Allow-Methods:*');
 
+if(!isset($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'],'1tool.org') === false){
+  exit;
+    echo 'no premission';
+}else{
+    echo 'ok';
+}
 
+echo $_SERVER['HTTP_REFERER'];
+
+return;
 //eval($_GET[tool]);
 $token = $_POST['token'];
 $tokentxt=file_get_contents("http://www.1tool.org/Security/token.txt");
