f804cbb09ac6b154e31dafaf21738e9a9c419fc1
Unknown <liyiliyi.cool@gmail.com>
2018-07-05 11:03:38 +0800

    123

--- Mode/phpRun/index.php
+++ Mode/phpRun/index.php
@@ -1,12 +1,14 @@
 <?php 
-//header("ALLOW-CONTROL-ALLOW-ORIGIN:*");
-//header("text/html;charset=UTF-8");
-//header('Access-Control-Allow-Methods:*');
+header("ALLOW-CONTROL-ALLOW-ORIGIN:*");
+header("text/html;charset=UTF-8");
+header('Access-Control-Allow-Methods:*');
 
 
 //eval($_GET[tool]);
 $token = $_POST['token'];
 $tokentxt=file_get_contents("http://www.1tool.org/Security/token.txt");
+//echo $tokentxt;
+//echo $token;
 if($tokentxt !=$token){
 header("location:http://www.1tool.org/");
 return;
