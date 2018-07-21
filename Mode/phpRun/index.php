4c58f6591ab702a820f70180b3c0699ec3a9f24a
Unknown <liyiliyi.cool@gmail.com>
2018-07-21 14:15:46 +0800

    123

--- Mode/phpRun/index.php
+++ Mode/phpRun/index.php
@@ -1,13 +1,43 @@
 <?php 
-header("ALLOW-CONTROL-ALLOW-ORIGIN:*");
-header("text/html;charset=UTF-8");
-header('Access-Control-Allow-Methods:*');
 
 if(!isset($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'],'1tool.org') === false){
   exit;
 }
-//echo $_SERVER['HTTP_REFERER'];
 
+$text = '<?php'.$_POST[tool].'?>';
+//检测非法函数
+$myArray = array("fopen","fwrite","mkdir","rmdir","unlink","eval");
+foreach($myArray as $key=>$val) {
+if(strpos($text,$val) !== false){ 
+ echo '包含'.$val.'非法函数'; 
+ exit;
+}
+}
+
+
+function isExist($file) {
+        $file = $_SERVER['DOCUMENT_ROOT'] . $file;
+        if (!file_exists($file)) {
+            mkdir($file);
+}
+}
+
+isExist('/cache/');
+
+
+$randData = rand(1,1000);
+$fp = fopen($_SERVER['DOCUMENT_ROOT'] .'/cache/'.$randData.'.php', "w");
+fwrite($fp, $text);
+fclose($fp);
+$f = "http://php-7.fast-page.org/cache/".$randData.".php";
+$result = file_get_contents($f);
+$result = str_replace("xlphp.net","1tool.org",$result);
+$result = str_replace("/xl_22311638","",$result);
+$result = str_replace("/cache","",$result);
+$result = str_replace("/php-7.fast-page.org","",$result);
+//$result = preg_replace("/([\s\S]*?)/","",$str);
+echo $result;
+//echo $_SERVER['HTTP_REFERER'];
 //return;
 //eval($_GET[tool]);
 //$token = $_POST['token'];
@@ -18,5 +48,6 @@ if(!isset($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'],'1tool.or
 //header("location:http://www.1tool.org/");
 //return;
 //}
-eval($_POST[tool]);
+//eval($_POST[tool]);  
+
 ?>
